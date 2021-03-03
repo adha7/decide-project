4c94c31d2b838563233120f47b2034f966d231b2
adha7 <adha.hrusto@systemverification.com>
2021-03-03 19:49:45 +0100

    feat: Implemented 4 tests for method LIC1 in a Decision class

--- test_interceptor_program.py
+++ test_interceptor_program.py
@@ -5,6 +5,7 @@ import launch_Parameters as Params
 import Connector as Con
 
 
+# TEST LIC0
 @pytest.mark.parametrize("length1,expected", [
     (4, 1),
     (6, 1),
@@ -22,3 +23,24 @@ def test_lic0(length1, expected):
 
     interceptor_system.parameters.length1 = length1
     assert interceptor_system.lic_0() == expected
+
+
+# TEST LIC1
+@pytest.mark.parametrize("points,radius1,expected", [
+    # Testing the boundaries
+    ([Point2D.Point2D(-1.0, 0.0), Point2D.Point2D(0.0, 1.0), Point2D.Point2D(1.0, 0.0)], -1, 0),
+    # Testing if LIC0 returns FALSE if three consecutive data points are within the circle
+    ([Point2D.Point2D(-1.0, 0.0), Point2D.Point2D(0.0, 1.0), Point2D.Point2D(1.0, 0.0)], 1.5, 0),
+    # Testing if LIC0 returns TRUE if three consecutive data points are outside of the circle
+    ([Point2D.Point2D(-2.0, 0.0), Point2D.Point2D(0.0, 2.0), Point2D.Point2D(2.0, 0.0)], 1.5, 1),
+    # Testing if LIC0 returns FALSE if three consecutive data points are within the circle of radius 10
+    ([Point2D.Point2D(-2.0, 0.0), Point2D.Point2D(0.0, 2.0), Point2D.Point2D(0.0, 0.0)], 10, 0),
+])
+# lic1 returns TRUE if three consecutive data points are not all contained within or on a circle of radius radius1
+def test_lic1(points, radius1, expected):
+    num_points = 3
+    parameters = Params.Parameters(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+    interceptor_system = lip.Decide(num_points, points, parameters, Con.Connector.ANDD, None)
+
+    interceptor_system.parameters.radius1 = radius1
+    assert interceptor_system.lic_1() == expected
